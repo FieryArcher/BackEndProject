30e692345bb92c8fb8bf7a48b39f6e1c30073723
FieryArcher <91003621+FieryArcher@users.noreply.github.com>
2022-04-23 08:50:27 +0600

    Delete SimpleBlogReadIt directory

--- SimpleBlogReadIt/ablog/theblog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    # path('', views.home, name="home"),
-    path('', Home.as_view(), name='home'),
-    path('article/<int:pk>', ArticleDetail.as_view(), name='article_detail'),
-    path('add_post/', AddPost.as_view(), name='add_post'),
-]
